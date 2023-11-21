4f52324f4f8c4d179762352760f3a02573d7392f
Arju Yadav <arjuy@gmail.com>
2023-11-21 02:06:28 +0000

    changes for training model

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from disease_classifier import logger
 from disease_classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from disease_classifier.pipeline.stage_02_base_model import BaseModelTrainingPipeline
-
+from disease_classifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 STAGE_NAME = "Data Ingestion stage"
 try:
     logger.info(f"======= stage {STAGE_NAME} started =======")
@@ -22,3 +22,14 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
